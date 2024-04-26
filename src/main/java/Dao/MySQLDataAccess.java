f8760cbabc1929e2883c8ce47601f00f62bf9298
BaoBao <dinhb140@gmail.com>
2024-04-26 18:14:19 +0700

    testconnect

---
+++ src/main/java/Dao/MySQLDataAccess.java
@@ -0,0 +1,36 @@
+package Dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class MySQLDataAccess {
+    // Thay thế thông tin sau với thông tin kết nối của bạn
+    static final String JDBC_URL = "jdbc:" + "mysql://gateway01.ap-southeast-1.prod.aws.tidbcloud.com:4000/TO_DO_LIST?sslMode=VERIFY_IDENTITY";
+    static final String USER = "349U3FBRT6ZmZme.root";
+    static final String PASSWORD = "oJEuK7kqGu6eEo9y";
+
+    public static void main(String[] args) {
+        try {
+            // Đăng ký driver JDBC
+            Class.forName("com.mysql.cj.jdbc.Driver");
+
+            // Mở kết nối
+            Connection connection = DriverManager.getConnection(JDBC_URL, USER, PASSWORD);
+
+            if (connection != null) {
+                 System.out.println("Kết nối đến cơ sở dữ liệu thành công!");
+                // Đóng kết nối sau khi sử dụng
+                connection.close();
+            } else {
+                System.out.println("Không thể kết nối đến cơ sở dữ liệu!");
+            }
+        } catch (ClassNotFoundException e) {
+            System.out.println("Không thể tìm thấy driver JDBC!");
+            e.printStackTrace();
+        } catch (SQLException e) {
+            System.out.println("Lỗi khi kết nối đến cơ sở dữ liệu!");
+            e.printStackTrace();
+        }
+    }
+}
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,6 +3,7 @@ module com.example.todolist {
     requires javafx.fxml;
 
     requires com.dlsc.formsfx;
+    requires java.sql;
     exports Gui;
     opens Gui to javafx.fxml;
     exports Dao;
