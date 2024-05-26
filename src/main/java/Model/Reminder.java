b9c775c83efdfe505da0ba0b41ef747328a766dd
trieu2003 <120072804+trieu2003@users.noreply.github.com>
2024-05-26 19:16:42 +0700

    thêm các class trong các Dao Controller Model

--- src/main/java/Model/Reminder.java
+++ src/main/java/Model/Reminder.java
@@ -3,11 +3,12 @@ package Model;
 import java.sql.Timestamp;
 
 public class Reminder {
-    private int reminderId;
-    private int taskId;
     private Timestamp reminderDate;
     private String reminderMessage;
     private boolean sent;
+    private int reminderId;
+    private int taskId;
+
 
     public Reminder(int reminderId, int taskId, Timestamp reminderDate, String reminderMessage) {
         this.reminderId = reminderId;
@@ -15,6 +16,27 @@ public class Reminder {
         this.reminderDate = reminderDate;
         this.reminderMessage = reminderMessage;
         this.sent = false;
+    }
+
+
+    public void setReminderDate(Timestamp reminderDate) {
+        this.reminderDate = reminderDate;
+    }
+
+    public String getReminderMessage() {
+        return reminderMessage;
+    }
+
+    public void setReminderMessage(String reminderMessage) {
+        this.reminderMessage = reminderMessage;
+    }
+
+    public boolean isSent() {
+        return sent;
+    }
+
+    public void setSent(boolean sent) {
+        this.sent = sent;
     }
 
     public int getReminderId() {
@@ -37,23 +59,4 @@ public class Reminder {
         return reminderDate;
     }
 
-    public void setReminderDate(Timestamp reminderDate) {
-        this.reminderDate = reminderDate;
-    }
-
-    public String getReminderMessage() {
-        return reminderMessage;
-    }
-
-    public void setReminderMessage(String reminderMessage) {
-        this.reminderMessage = reminderMessage;
-    }
-
-    public boolean isSent() {
-        return sent;
-    }
-
-    public void setSent(boolean sent) {
-        this.sent = sent;
-    }
 }
